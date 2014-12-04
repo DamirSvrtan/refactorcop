ed61588881f2b3d42a361f5f4f23fae53605f6fc
Maarten Claes <maartencls@gmail.com>
2014-12-04 19:27:59 +0100

    WIP Docker setup
    Install docker, boot2docker and fig
    
    https://docs.docker.com/installation/#installation
    http://www.fig.sh/install.html
    
    Then run:
    
      fig build
      fig up

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,19 @@
-# heroku redis url is set via : heroku config:set REDIS_PROVIDER=REDISGREEN_URL
-#
-require 'sidekiq/web'       #for background scheduler
+require 'sidekiq/web'
 
 Sidekiq::Web.use Rack::Auth::Basic, "Recovering Vegetarian Area" do |username, password|
   AdminAuthentication.call(username, password)
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV["REDISGREEN_URL"], size: 4 }
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    size: 4
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV["REDISGREEN_URL"], size: 1 }
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    size: 1
+  }
 end
